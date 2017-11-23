6be47a6e17ec3f9f715331bb4e015181e176dcb8
Bhanu <uabhanu@gmail.com>
2017-11-23 20:06:16 +0000

    Section 8 Lecture 193 Up Next

---
+++ BobBowler/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour 
+{
+	AudioSource m_ballSound;
+	Rigidbody m_ballBody;
+
+	[SerializeField] Vector3 m_launchVelocity;
+
+	void Start() 
+	{
+		m_ballBody = GetComponent<Rigidbody>();	
+		m_ballSound = GetComponent<AudioSource>();
+
+		Launch();
+	}
+
+	void Update() 
+	{
+		if(Time.timeScale == 0)
+		{
+			return;
+		}
+	}
+
+	public void Launch()
+	{
+		m_ballBody.velocity = m_launchVelocity;
+		m_ballSound.Play ();
+	}
+}
---
+++ BobBowler/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	Vector3 m_offset;
+
+	[SerializeField] Ball m_ball;
+
+	void Start()
+	{
+		m_offset = transform.position - m_ball.transform.position;	
+	}
+
+	void Update() 
+	{
+		if(Time.timeScale == 0)
+		{
+			return;
+		}
+
+		if(transform.position.z <= 1508f)
+		{
+			transform.position = m_ball.transform.position + m_offset;	
+		}
+	}
+}
