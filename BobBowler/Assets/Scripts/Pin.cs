b4b9642c4ed95bb6cd1889707f5280885e877173
Bhanu <uabhanu@gmail.com>
2017-11-25 12:47:18 +0000

    Section 8 Lecture 200 Finished

---
+++ BobBowler/Assets/Scripts/Pin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour 
+{
+	[SerializeField] float m_standingThreshold;
+
+	void Update() 
+	{
+		if(Time.timeScale == 0)
+		{
+			return;
+		}
+
+		IsStanding();
+	}
+
+	public bool IsStanding()
+	{
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+
+		float tiltX = Mathf.Abs(rotationInEuler.x);
+		float tiltZ = Mathf.Abs(rotationInEuler.z);
+
+		return(tiltX < m_standingThreshold || tiltZ < m_standingThreshold);
+	}
+}
---
+++ BobBowler/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour 
+{
+	[SerializeField] Text m_standingPinsDisplay;
+
+	void Start() 
+	{
+		
+	}
+
+	void Update() 
+	{
+		if(Time.timeScale == 0)
+		{
+			return;
+		}
+			
+		m_standingPinsDisplay.text = StandingPins().ToString();
+	}
+
+	int StandingPins()
+	{
+		int standingPins = 0;
+
+		foreach(Pin pin in FindObjectsOfType<Pin>())
+		{
+			if(pin.IsStanding())
+			{
+				standingPins++;
+			}
+		}
+
+		return standingPins;
+	}
+}
