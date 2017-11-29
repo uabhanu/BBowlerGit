c848dbb7771275bf21252d984723c2ed4ba83e33
Bhanu <uabhanu@gmail.com>
2017-11-29 11:51:00 +0000

    Section 8 Lecture 215 Up Next

--- BobBowler/Assets/Editor/BhanuActionTest.cs
+++ BobBowler/Assets/Editor/BhanuActionTest.cs
@@ -6,7 +6,17 @@ using System.Collections;
 
 public class BhanuActionTest 
 {
+    BhanuAction m_bhanuAction;
+    BhanuAction.Action m_endGame = BhanuAction.Action.ENDGAME;
     BhanuAction.Action m_endTurn = BhanuAction.Action.ENDTURN;
+    BhanuAction.Action m_reset = BhanuAction.Action.RESET;
+    BhanuAction.Action m_tidy = BhanuAction.Action.TIDY;
+
+    [SetUp]
+    public void Setup()
+    {
+        m_bhanuAction = new BhanuAction();
+    }
 
 	[Test]
 	public void T00PassingTest() 
@@ -17,8 +27,72 @@ public class BhanuActionTest
     [Test]
     public void T01OneStrikeReturnsEndTurn() 
     {
-        BhanuAction bhanuAction = new BhanuAction();
+        Assert.AreEqual(m_endTurn , m_bhanuAction.Bowl(10));
+    }
 
-        Assert.AreEqual(m_endTurn , bhanuAction.Bowl(10));
+    [Test]
+    public void T02Bowl8ReturnsTidy() 
+    {
+        Assert.AreEqual(m_tidy , m_bhanuAction.Bowl(8));
+    }
+
+    [Test]
+    public void T03Bowl28SpareReturnsEndTurn() 
+    {
+        m_bhanuAction.Bowl(8);
+        Assert.AreEqual(m_endTurn , m_bhanuAction.Bowl(2));
+    }
+
+    [Test]
+    public void T04CheckResetAtStrikeInLastFrame() 
+    {
+        int[] rolls =  {1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1};
+
+        foreach(int roll in rolls)
+        {
+            m_bhanuAction.Bowl(roll);
+        }
+
+        Assert.AreEqual(m_reset , m_bhanuAction.Bowl(10));
+    }
+
+    [Test]
+    public void T05CheckResetAtSpareInLastFrame() 
+    {
+        int[] rolls =  {1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1};
+
+        foreach(int roll in rolls)
+        {
+            m_bhanuAction.Bowl(roll);
+        }
+
+        m_bhanuAction.Bowl(1);
+        Assert.AreEqual(m_reset , m_bhanuAction.Bowl(9));
+    }
+
+    [Test]
+    public void T06YoutubeRollsEndInEndGame() 
+    {
+        int[] rolls =  {8,2 , 7,3 , 3,4 , 10 , 2,8 , 10 , 10 , 8,0 , 10 , 8,2};
+
+        foreach(int roll in rolls)
+        {
+            m_bhanuAction.Bowl(roll);
+        }
+
+        Assert.AreEqual(m_endGame , m_bhanuAction.Bowl(9));
+    }
+
+    [Test]
+    public void T07EndGameAtBowl20() 
+    {
+        int[] rolls =  {1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1,1 , 1};
+
+        foreach(int roll in rolls)
+        {
+            m_bhanuAction.Bowl(roll);
+        }
+
+        Assert.AreEqual(m_endGame , m_bhanuAction.Bowl(1));
     }
 }
--- BobBowler/Assets/Scripts/BhanuAction.cs
+++ BobBowler/Assets/Scripts/BhanuAction.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class BhanuAction
 {
-    public enum Action
-    {
-        TIDY,
-        RESET,
-        ENDTURN,    
-    };
+    int bowl = 1;
+    int[] bowls = new int[21];
+
+    public enum Action{TIDY , RESET , ENDTURN , ENDGAME};
 
 	public Action Bowl(int pins)
     {
-        return Action.ENDTURN;
+        if(pins < 0 || pins > 10){throw new UnityException("Sir Bhanu, Pins count is not equal to 10");}
+
+        bowls[bowl - 1] = pins;
+
+        if(bowl == 21)
+        {
+            return Action.ENDGAME;
+        }
+
+        if(bowl >= 19 && Bowl21Awarded())
+        {
+            bowl += 1;
+            return Action.RESET;
+        }
+
+        else if(bowl == 20 && !Bowl21Awarded())
+        {
+            return Action.ENDGAME;
+        }
+
+        if(pins == 10)
+        {
+            bowl += 2;
+            return Action.ENDTURN;
+        }
+
+        if(bowl % 2 != 0)
+        {
+            bowl += 1;
+            return Action.TIDY;
+        }
+
+        else if(bowl % 2 == 0)
+        {
+            bowl += 1;
+            return Action.ENDTURN;
+        }
+
+        throw new UnityException("Sir Bhanu, not sure what action to return");
+    }
+
+    bool Bowl21Awarded()
+    {
+        return(bowls[19-1] + bowls[20-1] >= 10);
     }
 }
