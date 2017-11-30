e9bc0f6e963cb1244a162beada738b64fb87b53b
Bhanu <uabhanu@gmail.com>
2017-11-30 18:41:07 +0000

    Section 8 Lecture 220 Up Next

---
+++ BobBowler/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager
+{
+    public static List<int> m_scoreFrames(List <int> rolls)
+    {
+        List<int> framesList = new List<int>();
+        return framesList;
+    }
+}
