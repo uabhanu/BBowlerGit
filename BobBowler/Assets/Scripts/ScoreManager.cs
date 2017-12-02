ca82c785103b669b2c6a9cb9bd24b7cbcd7f676f
Bhanu <uabhanu@gmail.com>
2017-12-02 00:40:22 +0000

    Section 8 Lecture 225 Up Next

--- BobBowler/Assets/Scripts/ScoreManager.cs
+++ BobBowler/Assets/Scripts/ScoreManager.cs
@@ -4,9 +4,49 @@ using UnityEngine;
 
 public class ScoreManager
 {
-    public static List<int> m_scoreFrames(List <int> rolls)
+    public static List<int> ScoreCumulative(List<int> rolls)
     {
-        List<int> framesList = new List<int>();
-        return framesList;
+        int runningTotal = 0;
+        List<int> cumulativeScores = new List<int>();
+
+        foreach(int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+    }
+
+    public static List<int> ScoreFrames(List<int> rolls)
+    {
+        List<int> frames = new List<int>();
+
+        for(int i = 1; i < rolls.Count; i += 2)
+        {
+            if(frames.Count == 10)
+                break;
+
+            if(rolls[i - 1] + rolls[i] < 10)
+            {
+                frames.Add(rolls[i - 1] + rolls[i]);   
+            }
+
+            if(rolls.Count - 1 <= 1)
+                break;
+
+            if(rolls[i - 1] == 10)
+            {
+                i--;
+                frames.Add(10 + rolls[i + 1] + rolls[i + 2]);
+            }
+
+            else if(rolls[i - 1] + rolls[i] == 10)
+            {
+                frames.Add(10 + rolls[i + 1]);
+            }
+        }
+
+        return frames;
     }
 }
