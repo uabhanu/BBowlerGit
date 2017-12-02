1f87b8d1e2a323d338acc87cd9259c369f83afe2
Bhanu <uabhanu@gmail.com>
2017-12-02 12:55:40 +0000

    Section 8 Lecture 229 Up Next

--- BobBowler/Assets/Scripts/GameManager.cs
+++ BobBowler/Assets/Scripts/GameManager.cs
@@ -7,18 +7,36 @@ public class GameManager : MonoBehaviour
     Ball m_ball;
     List<int> m_bowls = new List<int>();
     PinSetter m_pinSetter;
+    ScoreDisplay m_scoreDisplay;
 
 	void Start() 
     {
 		m_ball = FindObjectOfType<Ball>();
         m_pinSetter = FindObjectOfType<PinSetter>();
+        m_scoreDisplay = FindObjectOfType<ScoreDisplay>();
 	}
 
 	public void Bowl(int pinFall) 
     {
-		m_bowls.Add(pinFall);
-        BhanuAction.Action nextAction = BhanuAction.NextAction(m_bowls);
-        m_pinSetter.PerformAction(nextAction);
-        m_ball.Reset();
+        try
+        {
+            m_bowls.Add(pinFall);
+            BhanuAction.Action nextAction = BhanuAction.NextAction(m_bowls);
+            m_pinSetter.PerformAction(nextAction);
+            m_ball.Reset();
+        }
+        catch
+        {
+            Debug.LogWarning("Sir Bhanu, Something seems to be out of order, in Bowl() Method");
+        }
+
+        try
+        {
+            m_scoreDisplay.FillRollCard(m_bowls);
+        }
+        catch
+        {
+            Debug.LogWarning("Sir Bhanu, FillRollCard() Method failed for some reason");
+        }
 	}
 }
---
+++ BobBowler/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour 
+{
+    [SerializeField] Text[] m_frameTexts , m_rollTexts; 
+    
+	void Start() 
+    {
+		
+	}
+
+	void Update() 
+    {
+		
+	}
+
+    public void FillRollCard(List<int> rolls)
+    {
+        rolls.Add(11);    
+    }
+}   
